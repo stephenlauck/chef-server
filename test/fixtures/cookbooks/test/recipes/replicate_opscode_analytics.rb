9b7944688c0e29f4e8e5d9ec872bd2ccd1894a89
Stephen Lauck <stephen.lauck@gmail.com>
2015-05-08 17:16:47 -0400

    do not use tiered for analytics

--- test/fixtures/cookbooks/test/recipes/replicate_opscode_analytics.rb
+++ test/fixtures/cookbooks/test/recipes/replicate_opscode_analytics.rb
@@ -5,6 +5,6 @@ include_recipe 'test::setup_ssh_keys'
 case node['chef-server']['topology']
 when 'standalone'
   execute "scp -i /root/.ssh/insecure_private_key -o StrictHostKeyChecking=no -r root@33.33.33.10:/etc/opscode-analytics /etc/"
-when 'tiered'
+when 'tier'
   execute "scp -i /root/.ssh/insecure_private_key -o StrictHostKeyChecking=no -r root@33.33.33.12:/etc/opscode-analytics /etc/"
 end
