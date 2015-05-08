6ea16271135a961dbf15223ecd4308aa0d33fcef
Stephen Lauck <stephen.lauck@gmail.com>
2015-05-08 12:19:22 -0400

    update scp command to copy /etc/opscode dir to correct location on frontend

--- test/fixtures/cookbooks/test/recipes/replicate_etc_opscode.rb
+++ test/fixtures/cookbooks/test/recipes/replicate_etc_opscode.rb
@@ -2,4 +2,4 @@
 
 include_recipe 'test::setup_ssh_keys'
 
-execute "scp -i /root/.ssh/insecure_private_key -o StrictHostKeyChecking=no -r root@33.33.33.12:/etc/opscode /etc/opscode"
+execute "scp -i /root/.ssh/insecure_private_key -o StrictHostKeyChecking=no -r root@33.33.33.12:/etc/opscode /etc/"
